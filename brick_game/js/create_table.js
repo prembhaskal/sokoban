0584b2a03416f1fb2fe4946242fc6f2a43a57313
prembhaskal <pbhaskal@gmail.com>
2013-09-11 14:06:11 +0530

    added maze objects to maze.js.

--- brick_game/js/create_table.js
+++ brick_game/js/create_table.js
@@ -1,107 +1,17 @@
 // ***************** Creating Tables ****************************
 function createTestMaze(table) 
 {
-//	createMaze1(table);
+	var allMazeLevels = new AllMazeLevels();
+	var rawMaze = allMazeLevels.getRawMaze(1);
+	var maze = new Maze(rawMaze.mazeDimension, rawMaze.mazeStructure);
+	maze.init();
 	
-	var mazeLevel = new MazeLevels();
-	mazeLevel.setLevel(1);
-	
-	createMazeUsingLevel(mazeLevel, table);
+	createMazeUsingLevel(maze, table);
 }
 
-function createMazeUsingLevel(mazeLevel, table)
+function createMazeUsingLevel(maze, table)
 {
-	var canvas = new SokobanCanvas(mazeLevel.mazeDimension, table);
-	canvas.drawDefinedMaze(mazeLevel.pusherPosition, mazeLevel.brickArray,
-						   mazeLevel.destinationArray, mazeLevel.immovableArray);
+	var canvas = new SokobanCanvas(maze.mazeDimension, table);
+	canvas.drawDefinedMaze(maze.pusherPosition, maze.brickArray,
+						   maze.destinationArray, maze.immovableArray);
 }
-
-/*
-	E - empty_space
-	D - destination
-	B - brick
-	I - immovable
-	P - pusher
-	
-*/
-
-// TODO move it to a separate file.
-function MazeLevels() 
-{
-	this.mazeDimension = null;
-	this.pusherPosition = null;
-	this.emptySpaceArray = null;
-	this.destinationArray = null;
-	this.immovableArray = null;
-	this.brickArray = null;
-
-	/*
-	-------- Maze1 ---------
-	EDED
-	EBEB
-	EPIE
-	EEEE
-	*/
-	var level1 = [
-	              ['E','D','D','E'],
-	              ['E','B','B','I'],
-	              ['E','P','I','E'],
-	              ['E','E','E','E']
-	              ];
-	
-	// TODO get different levels based on the var 'level'.
-	this.getLevelStructure = function(levelNo)
-	{
-		return level1;
-	};
-}
-
-// TODO create raw mazelevel having just dimension and structure.
-
-// TODO remove hardcoded dimension
-MazeLevels.prototype.setLevel = function(levelNo)
-{
-	// initialize arrays and dimension
-	this.mazeDimension = new MazeDimension(4, 4);
-	this.emptySpaceArray = new Array();
-	this.destinationArray = new Array();
-	this.immovableArray = new Array();
-	this.brickArray = new Array();
-	
-	var mazeLevel = this.getLevelStructure(levelNo);
-	
-	for (var i = 0; i < 4; i++) 
-	{
-		for (var j = 0; j < 4; j++) 
-		{
-			var type = mazeLevel[i][j];
-			var position = new Position(j,3-i);
-			
-			if (type == 'E')
-			{
-				this.emptySpaceArray.push(position);
-			} 
-			else if (type == 'B')
-			{
-				this.brickArray.push(position);
-			}
-			else if (type == 'D')
-			{
-				this.destinationArray.push(position);
-			}
-			else if (type == 'I')
-			{
-				this.immovableArray.push(position);
-			} 
-			else if (type == 'P')
-			{
-				this.pusherPosition = position;
-			}
-				
-			
-		}
-	}
-};
-
-
-
--- brick_game/js/maze.js
+++ brick_game/js/maze.js
@@ -3,3 +3,122 @@ function MazeDimension(width, height) {
 	this.width = width;
 	this.height = height;
 }
+
+function RawMaze(mazeDimension, mazeStructure)
+{
+	this.mazeDimension = mazeDimension;
+	this.mazeStructure = mazeStructure;
+}
+
+function Maze(mazeDimension, mazeStructure)
+{
+	this.mazeDimension = mazeDimension;
+	this.mazeStructure = mazeStructure;
+	
+	this.pusherPosition = null;
+	this.emptySpaceArray = null;
+	this.destinationArray = null;
+	this.immovableArray = null;
+	this.brickArray = null;
+	
+	// TODO initialise the maze on during construction.
+	this.init = function ()
+	{
+		// initialise arrays and dimension
+		this.emptySpaceArray = new Array();
+		this.destinationArray = new Array();
+		this.immovableArray = new Array();
+		this.brickArray = new Array();
+		
+		for (var i = 0; i < this.mazeDimension.height; i++) 
+		{
+			for (var j = 0; j < this.mazeDimension.width; j++) 
+			{
+				var type = this.mazeStructure[i][j];
+				var position = new Position(j, this.mazeDimension.height-1-i);
+				
+				if (type == 'E')
+				{
+					this.emptySpaceArray.push(position);
+				} 
+				else if (type == 'B')
+				{
+					this.brickArray.push(position);
+				}
+				else if (type == 'D')
+				{
+					this.destinationArray.push(position);
+				}
+				else if (type == 'I')
+				{
+					this.immovableArray.push(position);
+				} 
+				else if (type == 'P')
+				{
+					this.pusherPosition = position;
+				}
+					
+				
+			}
+		}
+	};
+
+}
+
+
+// Object containing all the levels of the maze.
+function AllMazeLevels() 
+{
+	var rawMazes = new Array();
+	
+	addAllMazes();
+	
+	function addAllMazes()
+	{
+		/*
+		  Basic terminologies
+			E - empty_space
+			D - destination
+			B - brick
+			I - immovable
+			P - pusher
+			
+			-------- Maze1 ---------
+			EDED
+			EBEB
+			EPIE
+			EEEE
+		*/
+		var dimension1 = new MazeDimension(4, 4);
+		var level1 = [
+		              ['E','D','D','E'],
+		              ['E','B','B','I'],
+		              ['E','P','I','E'],
+		              ['E','E','E','E']
+		              ];
+		
+		var rawMaze1 = new RawMaze(dimension1, level1);
+		rawMazes.push(rawMaze1);
+		
+		/*
+			-------- Maze2 ---------
+			EEDD
+			EBPB
+			EIIE
+		*/
+		var dimension2 = new MazeDimension(4, 3);
+		var level2 = [
+		              ['E','E','D','D'],
+		              ['E','B','P','B'],
+		              ['E','I','I','E'],
+		              ];
+		
+		var rawMaze2 = new RawMaze(dimension2, level2);
+		rawMazes.push(rawMaze2);
+	};
+	
+	// levelNo is 1 based, hence -1
+	this.getRawMaze = function(levelNo) {
+		return rawMazes[levelNo-1];
+	};
+}
