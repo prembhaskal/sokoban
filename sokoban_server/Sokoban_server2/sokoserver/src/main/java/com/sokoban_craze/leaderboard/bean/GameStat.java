48544b1e992efb0f896f6c00800fe324ef1665f3
gauchy <ashwini.rai13@gmail.com>
2014-06-04 11:00:00 +0530

    Porting files from prev version of server

---
+++ sokoban_server/Sokoban_server2/sokoserver/src/main/java/com/sokoban_craze/leaderboard/bean/GameStat.java
@@ -0,0 +1,28 @@
+package com.sokoban_craze.leaderboard.bean;
+
+public class GameStat {
+	
+	private String userName;
+	private int moves;
+	private float time;
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public int getMoves() {
+		return moves;
+	}
+	public void setMoves(int moves) {
+		this.moves = moves;
+	}
+	public float getTime() {
+		return time;
+	}
+	public void setTime(float time) {
+		this.time = time;
+	}
+	
+
+}
